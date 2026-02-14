7505023f52f95820be24b51820340f19c54cf456
Devansh Singh <rajputdevansh49@gmail.com>
2026-02-14 16:44:27 +0530

    Data Transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -14,8 +14,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass 
 
-#from src.components.data_transformation import DataTransformation
-#from src.components.data_transformation import DataTransformationConfig
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 #from src.components.model_trainer import ModelTrainerConfig
 #from src.components.model_trainer import ModelTrainer
@@ -46,7 +46,7 @@ class DataIngestion:
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
-            logging.info("Inmegestion of the data iss completed")
+            logging.info("Ingestion of the data is completed")
 
             return(
                 self.ingestion_config.train_data_path,
@@ -60,8 +60,8 @@ if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
 
-    #data_transformation=DataTransformation()
-    #train_arr,test_arr,_=data_transformation.initate_data_transformation(train_data,test_data)
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
 
-    #= ModelTrainer()
-    #print(modeltrainer.inititate_model_trainer(train_arr,test_arr))
+    #== ModelTrainer()
+    #=print(modeltrainer.inititate_model_trainer(train_arr,test_arr))
